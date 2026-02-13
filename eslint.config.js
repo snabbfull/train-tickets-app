00105beb388c345d92df12e8201523d1b1d31df1
Игорь Дмитриев <igor_dmitriev@MacBook-Air-de-Igor.local>
2026-02-13 16:11:41 +0300

    Change app

--- eslint.config.js
+++ eslint.config.js
@@ -1,49 +1,29 @@
-import js from "@eslint/js";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
-import globals from "globals";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  // Игнорировать папки
-  { ignores: ["dist", "node_modules", "build"] },
-
-  // Основная конфигурация
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      parser: tsParser,
+      ecmaVersion: 2020,
+      globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
-      globals: {
-        ...globals.browser, // document, window и т.д.
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      react: reactPlugin,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
-      ...reactPlugin.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-    },
-    settings: {
-      react: { version: "detect" },
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-
-  // Конфигурация для тестов (Jest)
-  {
-    files: ["**/*.{test,spec}.{js,jsx,ts,tsx}"],
-    languageOptions: {
-      globals: {
-        ...globals.jest, // test, expect, describe и т.д.
-      },
-    },
-  },
-];
+])
